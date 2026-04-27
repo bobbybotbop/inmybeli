cb9fb8872aebec9ae64598a8dade606476df34ed
Ronald Leung <ronaldfleung@gmail.com>
2026-04-26 20:52:50 -0400

    initialize xcode project

---
+++ frontend/InMyBeli/InMyBeli/InMyBeliApp.swift
@@ -0,0 +1,17 @@
+//
+//  InMyBeliApp.swift
+//  InMyBeli
+//
+//  Created by Ronald on 4/26/26.
+//
+
+import SwiftUI
+
+@main
+struct InMyBeliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
